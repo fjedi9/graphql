f877e422d1af601691e60f125e6434fd873b239f
Felipe Vieira <vieira.felipe@gmail.com>
2020-07-16 14:35:56 -0300

    comitando

---
+++ src/test/java/com/vepilef/compras/ComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vepilef.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
